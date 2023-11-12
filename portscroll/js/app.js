e25c91a75f442b8489f7246f03a2a5fc7a66c995
SarmaGreen <65596536+SarmaGreen@users.noreply.github.com>
2023-11-13 02:16:36 +1000

    454

---
+++ portscroll/js/app.js
@@ -0,0 +1,7 @@
+const slider1 = new Swiper('.slider1' {
+        freeMode: true,
+        centeredSlides: true,
+        direction: 'vertical',
+        mousewheel:true,
+        slidesPerView: 1.75,
+})
