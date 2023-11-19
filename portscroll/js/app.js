2bcf7137ada3a3c62e8d89f7d48a0f01fa4d5e6f
SarmaGreen <65596536+SarmaGreen@users.noreply.github.com>
2023-11-19 23:40:03 +1000

    3123123

--- portscroll/js/app.js
+++ portscroll/js/app.js
@@ -1,9 +1,10 @@
 document.querySelectorAll('.slider').forEach((n, i) => {
-  window[`slider${i+1}`] = new Swiper(n, {
-    freeMode: true,
-    centeredSlides: true,
-    direction: 'vertical',
-    mousewheel:true,
-    slidesPerView: 1.75,
-  })
+	window[`slider${i+1}`] = new Swiper(n, {
+		freeMode: true,
+		centeredSlides: true,
+		direction: 'vertical',
+		mousewheel: true,
+		slidesPerView: 1.75,
+		slidesOffsetBefore: -125
+	})
 })
